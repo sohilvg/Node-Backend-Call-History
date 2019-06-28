4f4315eca30dee0082a9899fb06f6ad4454a457e
SohilVg <sohilpatel529@gmail.com>
2019-06-28 18:06:14 +0530

    auth login

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,11 +1,12 @@
 const knex = require("knex")({
     client: "postgres",
-    connection:process.env.DATABASE_URL,
-    pool: {
-       min:2,
-       max:10
-    },
-    ssl : true,
-    debug: false
-});
+        connection:process.env.DATABASE_URL,
+        pool: {
+           min:2,
+           max:10
+        },
+        ssl : true,
+        debug: false
+    });
+
 module.exports = knex;
